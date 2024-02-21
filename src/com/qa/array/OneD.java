1fe6134685671fc1621ccfcfc4d8a73555384716
khondokar habibullah <shohelhabib1989@gmail.com>
2024-02-20 19:45:49 -0500

    2nd commit

--- src/com/qa/array/OneD.java
+++ src/com/qa/array/OneD.java
@@ -3,12 +3,13 @@ package com.qa.array;
 public class OneD {
 	
 	public static void main(String[]arg) {
-		int []arr= {10,20,30,40,50};
-//		arr[0]=10;
-//		arr[1]=20;
-//		arr[2]=30;
-//		arr[3]=40;
-//		arr[4]=50;
+		//int []arr= {10,20,30,40,50};
+		int [] arr=new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
 		
 		for(int i=0; i<arr.length; i++) {
 			
